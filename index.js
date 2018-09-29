2af8e4fcbe90e82c64d1d54d1e0a0662ff0fc1a0
Brighton Tanvir <brightontanvir@gmail.com>
2018-09-29 03:53:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,6 @@ function happyHolidays() {
  function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 };
+ function holidayCountdown(holiday, number) {
+  return `It\'s ${number} days until ${holiday}!`;
+}
